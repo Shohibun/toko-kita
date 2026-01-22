d87c68e1ebc2cfa55534e603efa9d933b9719ef2
shohibunnajam <shohibunnajam@gmail.com>
2026-01-22 20:33:17 +0700

    responsive auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,6 +2,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
+    <!-- Form Login -->
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
@@ -17,9 +18,9 @@
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                type="password"
+                name="password"
+                required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -32,16 +33,24 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
+        <div class="flex items-center justify-between mt-4">
+            <a
+                href="{{ route('register') }}"
+                class="text-sm text-blue-600 hover:underline">
+                Register
+            </a>
+
+            <div class="">
+                @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
+                @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+                <x-primary-button class="ms-3">
+                    {{ __('Log in') }}
+                </x-primary-button>
+            </div>
         </div>
     </form>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,7 +22,7 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="bg-white w-8/12 shadow-lg overflow-hidden rounded-lg mt-6 px-6 py-4 md:w-4/12">
                 {{ $slot }}
             </div>
         </div>
